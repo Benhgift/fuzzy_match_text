b31a9e508467821ff3fbcbe466c3894364142b4e
Ben Gift <corn8bit@gmail.com>
2013-06-19 11:07:55 -0700

    now it can find best non overlapping paths

--- dag.py
+++ dag.py
@@ -1,4 +1,6 @@
 from pprint import pprint
+import sys
+from operator import itemgetter
 import csv
 
 COL = 0
@@ -6,13 +8,18 @@ ROW = 1
 
 class Node(object):
     """docstring for Node"""
-    def __init__(self, weight, parent, match):
-
+    def __init__(self, weight=0, parent=[], match=0):
         self.weight = weight
         # parent is (x,y) coord
         self.parent = parent
-
         self.match = match
+
+class contig_node(Node):
+    """this represents a contig in a special DAG"""
+    def __init__(self, contig_path, weight, parent_node=[]):
+        self.weight = weight
+        self.parent_node = parent_node
+        self.contig_path = contig_path
 
 class DAG(object):
     """docstring for DAG"""
@@ -24,12 +31,13 @@ class DAG(object):
         self.num_cols = len(self.list2)
 
         self.dag_array = None
+        self.multi_paged_array = None
+        self.list_of_contigs = None
         self.NON_MATCH_CHAR = '#'
         self.match_weight = 10
         self.miss_weight = 30
         self.indel = 20
         self.word_size = self.match_weight*4
-        self.list_of_contigs = 0
 
     def compute_all_long_contigs(self):
         self.compare ()
@@ -40,7 +48,7 @@ class DAG(object):
         for col in range(self.num_cols -1, 0, -1):
             for row in range(self.num_cols -1, 0, -1):
                 self.get_contig_if_missing(col, row, list_of_contigs)
-        self.list_of_contigs = [contigs for contigs in list_of_contigs if not contigs is []]
+        self.list_of_contigs = [contigs for contigs in list_of_contigs if contigs]
 
     def remove_postfix_mismatches_from_contigs(self, contigs):
         if len(contigs) < 2:
@@ -232,8 +240,11 @@ class DAG(object):
         print("Bottom right: {}".format(self.get_string((self.num_rows - 1, self.num_rows - 1))))
 
     def print_contigs(self):
+        self.print_contigs_from_list(self.list_of_contigs)
+
+    def print_contigs_from_list(self, list_of_contigs):
         print "\nContigs found:"
-        for contig in self.list_of_contigs:
+        for contig in list_of_contigs:
             if contig:
                 #print contig
                 text = [self.list2[y] for x,y in contig]
@@ -244,7 +255,7 @@ class DAG(object):
         score = 0
         for contig in self.list_of_contigs:
             if contig:
-              score += self.get(contig[0]).weight
+                score += self.get(contig[0]).weight
         print "\nScore (total match score divided by string len): ",
         print score / ((len(self.list1) +len(self.list2)) / 2)
 
@@ -254,6 +265,47 @@ class DAG(object):
             writer.writerow([''] + self.list2)
             for index, row in enumerate(self.dag_array):
                 writer.writerow([self.list1[index]] + [node.weight for node in row])
+
+    def create_best_non_overlapping_path(self):
+        # line up contigs for ref strand (x axis)
+        sorted_contigs = sorted(self.list_of_contigs, key = lambda tup: tup[-1][-1])
+        contig_dag = [contig_node(contig, self.get(contig[0]).weight) for contig in sorted_contigs]
+        # for each contig, 
+        for contig in contig_dag:
+            # look back and pick the best contig, and add the score to this
+            best_parent = self.get_best_parent_contig(contig_dag, contig)
+            if best_parent: 
+                contig.weight += best_parent.weight
+                contig.parent_node = best_parent
+        # highest contig score is winner, print it and its path
+        best_contig = max(contig_dag, key=lambda cont: cont.weight)
+        self.print_contig_path(best_contig)
+        return
+
+    def get_best_parent_contig(self, sorted_contigs, this_contig):
+        possible_parents = self.get_possible_parent_contigs(sorted_contigs, this_contig)
+        best_parent = None
+        if possible_parents:
+            best_parent = max(possible_parents, key=lambda par: par.weight)
+        return best_parent
+
+    def get_possible_parent_contigs(self, sorted_contigs, this_contig):
+        # skip any contigs which overlap on cur_strand (ex: 1 "the" matching 2 places)
+        possible_parents = [a_contig for a_contig in sorted_contigs if a_contig.contig_path[0][-1] < this_contig.contig_path[-1][-1]]
+        return possible_parents
+
+    def print_contig_path(self, contig):
+        print "\nBest contig path score: " + str(contig.weight)
+        temp_contig = contig
+        contigs_list = []
+        if not contig:
+            return
+        while temp_contig:
+            #print temp_contig.contig_path
+            contigs_list.append(temp_contig.contig_path)
+            temp_contig = temp_contig.parent_node
+        self.print_contigs_from_list(contigs_list)
+        return
 
 if __name__ == '__main__':
     string1 = list("The mouse is a house, and I ate it")
@@ -265,4 +317,5 @@ if __name__ == '__main__':
 
     dag.print_contigs()
     dag.print_score()
+    dag.create_best_non_overlapping_path()
     dag.save_csv("dag.csv")
